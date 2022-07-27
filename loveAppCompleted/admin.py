ca91c1ece8e8d08226130fb5eacd5d17fb376718
sonune <99712141+sonune@users.noreply.github.com>
2022-07-27 19:03:05 +0530

    Update admin.py

--- loveAppCompleted/admin.py
+++ loveAppCompleted/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
-from .models import loveChatModel
+#from .models import loveChatModel
+from .models import loveChatModel, oldChats1, oldChats2, oldChats3
 
 # Register your models here.
 
 @admin.register(loveChatModel)
 class loveChatModelAdmin(admin.ModelAdmin):
+    list_display = ['id','message','timestamp','user']
+    
+@admin.register(oldChats1)
+class oldChats1Admin(admin.ModelAdmin):
+    list_display = ['id','message','timestamp','user']
+
+
+@admin.register(oldChats2)
+class oldChats2Admin(admin.ModelAdmin):
+    list_display = ['id','message','timestamp','user']
+
+
+@admin.register(oldChats3)
+class oldChats3Admin(admin.ModelAdmin):
     list_display = ['id','message','timestamp','user']
